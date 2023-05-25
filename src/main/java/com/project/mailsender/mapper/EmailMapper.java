8cd80c91e31b0dbf67946f3457720c17a35ad5d8
Lara Menck <lara.menckt@gmail.com>
2023-05-24 21:48:48 -0300

    no longer using lombok

--- src/main/java/com/project/mailsender/mapper/EmailMapper.java
+++ src/main/java/com/project/mailsender/mapper/EmailMapper.java
@@ -1,19 +1,22 @@
 package com.project.mailsender.mapper;
 
-import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import com.project.mailsender.dtos.EmailDTO;
 import com.project.mailsender.model.Email;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class EmailMapper {
 
     private final ModelMapper mapper;
+
+    public EmailMapper(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
 
     public Email toEmail(EmailDTO dto) {
         return mapper.map(dto, Email.class);
