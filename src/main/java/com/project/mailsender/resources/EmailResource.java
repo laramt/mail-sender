5d60113a5595465983a471cc11b2ae2ef85ee966
Lara Menck <lara.menckt@gmail.com>
2023-06-14 09:18:39 -0300

    email resource refactor

--- src/main/java/com/project/mailsender/resources/EmailResource.java
+++ src/main/java/com/project/mailsender/resources/EmailResource.java
@@ -5,7 +5,6 @@ import com.project.mailsender.services.EmailService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,43 +16,42 @@ import java.util.List;
 @RequestMapping("/email")
 public class EmailResource {
 
-    
    private final EmailService service;
+
 
     @PostMapping("/send-email")
     public ResponseEntity<EmailDTO> sendEmail(@RequestBody EmailDTO dto) throws MessagingException {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.sendTemplateEmail(dto));
+        return ResponseEntity.created(null).body(service.sendTemplateEmail(dto));
     }
 
     @PostMapping("/send-template-email")
     public ResponseEntity<EmailDTO> sendTemplateEmail(@RequestBody EmailDTO dto) throws MessagingException {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.sendTemplateEmail(dto));
+        return ResponseEntity.created(null).body(service.sendTemplateEmail(dto));
     }
 
     @PostMapping("/send-email-attachment-template")
     public ResponseEntity<EmailDTO> sendTemplateEmailwithAttachment(@RequestBody EmailDTO dto) throws MessagingException {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.sendTemplateEmailwithAttachment(dto));
+        return ResponseEntity.created(null).body(service.sendTemplateEmailwithAttachment(dto));
     }
 
     @GetMapping("/emails")
     public ResponseEntity<List<EmailDTO>> getAllEmails() {
-        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+        return ResponseEntity.ok().body(service.findAll());
     }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<EmailDTO> getEmailById(@PathVariable(name = "id") Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
     @GetMapping("/{recipientName}")
     public ResponseEntity<List<EmailDTO>> getByRecipientName(String recipientName) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.findByRecipientName(recipientName));
+        return ResponseEntity.ok().body(service.findByRecipientName(recipientName));
     }
 
      @GetMapping("/{sendDateEmail}")
     public ResponseEntity<List<EmailDTO>> getBySendDate(LocalDateTime sendDateEmail) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.findBySendDateEmail(sendDateEmail));
+        return ResponseEntity.ok().body(service.findBySendDateEmail(sendDateEmail));
     }
 
 }
